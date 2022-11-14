182a77fc2e09056b9c6512a82fb96a18eb2e6b75
ChristianAk <christian2022.kodehode@gmail.com>
2022-11-14 10:57:38 +0100

    Fixed some console errors.

--- src/components/Incrementor/Incrementor.js
+++ src/components/Incrementor/Incrementor.js
@@ -8,9 +8,9 @@ function Incrementor() {
 
 		<h1>There has been {i} days since last accident</h1>
 
-		<div class="actions">
-			<button onClick={() => increment(--i)} class="actions__button actions__button--decrease">Decrease Count</button>
-			<button onClick={() => increment(++i)} class="actions__button actions__button--increase">Increase Count</button>
+		<div className="actions">
+			<button onClick={() => increment(--i)} className="actions__button actions__button--decrease">Decrease Count</button>
+			<button onClick={() => increment(++i)} className="actions__button actions__button--increase">Increase Count</button>
 		</div>
 
 		</>
