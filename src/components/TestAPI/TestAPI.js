78b4c5f0951942d16fb602cf67a022aebf4ff9a1
ChristianAk <christian2022.kodehode@gmail.com>
2022-11-23 10:34:25 +0100

    Bugfixes.

--- src/components/TestAPI/TestAPI.js
+++ src/components/TestAPI/TestAPI.js
@@ -1,25 +1,31 @@
 import { useState, useEffect } from "react"
 
+let renderCount = 0;
+
 function TestAPI() {
+
 	const [ data, setData ] = useState()
 
 	useEffect(() => {
+		console.log(`useEffect called ${++renderCount} times. `)
 		async function dataFetch(callback) {
-			const data = await fetch("https://kanjiapi.dev/v1/kanji/grade-1")
-			.then(response => response.json())
-			.then(response => callback(response))
+			const response = await fetch("https://kanjiapi.dev/v1/kanji/grade-1")
+			const data = await response.json()
+			console.log("api http status:",response.status)
+			callback(data)
 		}
-		dataFetch(console.log)
 		dataFetch(setData)
 	},
 	[])
-	
-	return (
-		<div style={{textAlign: "center"}}>
-			<h1 style={{fontSize: 150+"%"}}>Japanese character for learning:</h1>
-			<p style={{fontSize: 300+"%"}}>{data[10]}</p>
-		</div>
-	)
+
+	if (data) {
+		return (
+			<div style={{textAlign: "center"}}>
+				<h1 style={{fontSize: 150+"%"}}>Japanese character for learning:</h1>
+				<p style={{fontSize: 300+"%"}}>{data[10]}</p>
+			</div>
+		)
+	}
 }
 
 export default TestAPI
